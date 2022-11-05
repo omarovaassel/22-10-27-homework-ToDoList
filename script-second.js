2c8348574358e13b5c3b2cabab052a0e5e7d0963
omarovaassel <omarova.assel@gmail.com>
2022-11-05 13:37:31 +0600

    ToDoList homework was added

---
+++ script-second.js
@@ -0,0 +1,49 @@
+const drawTasks = () => {
+    let listSection = document.querySelector("#list_section");
+    listSection.innerHTML = ""; // очищает список
+
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    for(let i = 0; i < tasks.length; i++){
+        listSection.innerHTML += `
+            <div class="list_item row">
+                <div class="row" style="margin: 0">
+                    <input type="checkbox" class="task_checkbox" ${tasks[i].done ? "checked" : ""} onClick='onChange(this)'>  <!-- сохраняем галочки на задачах и показываем их -->
+                    <span class="task_text"> ${tasks[i].text} </span>
+                </div>
+
+                <div class="row" style="margin: 0; gap: 10px">
+                    <button class="btn red-bg" onclick="removeTask(${i})">x</button> <!--  i в этом случае будет давать возможность определить какой индекс у элемента, который мы хотим удалить -->
+                    <button class="btn yellow-bg" onclick="editTask(${i})">...</button>
+                </div>
+            </div>
+        `;
+    }
+}
+
+drawTasks();
+
+// удаление задачи
+function removeTask(i){
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.splice(i, 1); //удалить из массива элемент с индексом, который в строке 20
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    drawTasks();
+}
+
+
+//сохранение статуса checkbox
+function save(on){
+    localStorage.setItem('task_checkbox', on.toString());
+}
+
+function load(){
+    return localStorage.getItem('task_checkbox') === 'true';
+}
+
+function onChange(checkbox){
+    const value = checkbox.checked;
+    save(value);
+}
+
+const initialValue = load();
+document.querySelector('.task_checkbox').checked = initialValue;
